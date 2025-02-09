e918b0be23598cb8cfb2a9623c37590a095977a2
Tom <tjp.salet@gmail.com>
2025-02-09 15:04:35 +0100

    Minimal skeleton

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-class ToggleCardWithShadowDom extends HTMLElement {
+class RecipeCard extends HTMLElement {
 
     // private properties
     _config;
@@ -12,52 +12,16 @@ class ToggleCardWithShadowDom extends HTMLElement {
         this.doStyle();
         this.doAttach();
         this.doQueryElements();
-        this.doListen();
     }
 
     setConfig(config) {
         this._config = config;
         this.doCheckConfig();
-        this.doUpdateConfig();
     }
 
     set hass(hass) {
         this._hass = hass;
-        this.doUpdateHass()
-    }
-
-    onClicked() {
-        this.doToggle();
-    }
-
-    // accessors
-    isOff() {
-        return this.getState().state === 'off';
-    }
-
-    isOn() {
-        return this.getState().state === 'on';
-    }
-
-    getHeader() {
-        return this._config.header;
-    }
-
-    getEntityID() {
-        return this._config.entity;
-    }
-
-    getState() {
-        return this._hass.states[this.getEntityID()];
-    }
-
-    getAttributes() {
-        return this.getState().attributes
-    }
-
-    getName() {
-        const friendlyName = this.getAttributes().friendly_name;
-        return friendlyName ? friendlyName : this.getEntityID();
+        // TODO fill card
     }
 
     // jobs
@@ -70,66 +34,48 @@ class ToggleCardWithShadowDom extends HTMLElement {
     doCard() {
         this._elements.card = document.createElement("ha-card");
         this._elements.card.innerHTML = `
-                <div class="card-content">
-                    <p class="error error hidden">
-                    <dl class="dl">
-                        <dt class="dt"></dt>
-                        <dd class="dd">
-                            <span class="toggle">
-                                <span class="button"></span>
-                            </span>
-                            <span class="value">
-                            </span>
-                        </dd>
-                    </dl>
-                </div>
-        `;
+            <div class="selectdiv"></div>
+            <div class="content"></div>
+        `
     }
-
 
     doStyle() {
         this._elements.style = document.createElement("style");
         this._elements.style.textContent = `
-            .error {
-                text-color: red;
+            .selectdiv {
+                margin: 10px
             }
-            .error.hidden { display: none; }
-            .dl {
-                display: grid;
-                grid-template-columns: repeat(2, minmax(0, 1fr));
+            .selectdiv > select {
+                width: 100%;
+                padding: 5px;
+                border-radius: 10px;
             }
-            .dl.hidden { display: none; }
-            .dt {
-                display: flex;
-                align-content: center;
-                flex-wrap: wrap;
+            .content {
+                padding: 16px;
+                font-family: Calibri
             }
-            .dd {
-                display: grid;
-                grid-template-columns: repeat(2, minmax(0, auto) minmax(0, 2fr));
-                margin: 0;
+            .recipe-title {
+              font-size: 1.5em;
+              font-weight: bold;
+              margin-bottom: 10px;   
+              border-bottom: black 1px solid;
+              padding-bottom: 5px;
+              font-family: Cambria;
             }
-            .toggle {
-                padding: 0.6em;
-                border: grey;
-                border-radius: 50%;
+            .recipe-content{
+              margin-left:20px;
             }
-            .toggle.on { background-color: green; }
-            .toggle.off{ background-color: red; }
-            .button {
-                display: block;
-                border: outset 0.2em;
-                border-radius: 50%;
-                border-color: silver;
-                background-color: silver;
-                width: 1.4em;
-                height: 1.4em;
+            .ingredient-list { 
+              padding-inline-start: 20px;
+              margin: 0;
             }
-            .value {
-                padding-left: 0.5em;
-                display: flex;
-                align-content: center;
-                flex-wrap: wrap;
+            .ingredient {
+            }
+            .amount {
+            }
+            .instruction-list {
+              padding-inline-start: 20px;
+              margin: 0;
             }
         `
     }
@@ -141,64 +87,9 @@ class ToggleCardWithShadowDom extends HTMLElement {
 
     doQueryElements() {
         const card = this._elements.card;
-        this._elements.error = card.querySelector(".error")
-        this._elements.dl = card.querySelector(".dl")
-        this._elements.topic = card.querySelector(".dt")
-        this._elements.toggle = card.querySelector(".toggle")
-        this._elements.value = card.querySelector(".value")
+        this._elements.selectdiv = card.querySelector(".selectdiv")
+        this._elements.content = card.querySelector(".content");
     }
-
-    doListen() {
-        this._elements.dl.addEventListener("click", this.onClicked.bind(this), false);
-    }
-
-    doUpdateConfig() {
-        if (this.getHeader()) {
-            this._elements.card.setAttribute("header", this.getHeader());
-        } else {
-            this._elements.card.removeAttribute("header");
-        }
-    }
-
-    doUpdateHass() {
-        if (!this.getState()) {
-            this._elements.error.textContent = `${this.getEntityID()} is unavailable.`;
-            this._elements.error.classList.remove("hidden");
-            this._elements.dl.classList.add("hidden");
-        } else {
-            this._elements.error.textContent = "";
-            this._elements.topic.textContent = this.getName();
-            if (this.isOff()) {
-                this._elements.toggle.classList.remove("on");
-                this._elements.toggle.classList.add("off");
-            } else if (this.isOn()) {
-                this._elements.toggle.classList.remove("off");
-                this._elements.toggle.classList.add("on");
-            }
-            this._elements.value.textContent = this.getState().state;
-            this._elements.error.classList.add("hidden");
-            this._elements.dl.classList.remove("hidden");
-        }
-    }
-
-    doToggle() {
-        this._hass.callService('input_boolean', 'toggle', {
-            entity_id: this.getEntityID()
-        });
-    }
-
-    // configuration defaults
-    static getStubConfig() {
-        return { entity: "input_boolean.tcwsd" }
-    }
-
 }
 
-customElements.define('toggle-card-with-shadow-dom', ToggleCardWithShadowDom);
-
-window.customCards = window.customCards || [];
-window.customCards.push({
-    type: "toggle-card-with-shadow-dom",
-    name: "Vanilla Js Toggle With Shadow DOM",
-    description: "Turn an entity on and off"
-});
+customElements.define('recipe-card', RecipeCard);
